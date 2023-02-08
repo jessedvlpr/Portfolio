2e1389d1a982e32da6afa8d9f162c489a7f48ad4
Jesse Groves <grovesjesse@gmail.com>
2023-02-08 18:50:48 -0500

    re-pathed thumbnails

--- js/data.js
+++ js/data.js
@@ -9,7 +9,7 @@ data = `
             "school"
         ],
         "description": "This project was my first real venture into the Python programming language, it is a Tetris game made using the Pygame library, for a school project.",
-        "thumbnail": "./resources/Pythtris.png",
+        "thumbnail": "./thumbnails/Pythtris.png",
         "weblink": "",
         "repolink": "https://github.com/jessegroves123/Pythtris"
     },
@@ -21,7 +21,7 @@ data = `
             "website"
         ],
         "description": "You're lookin' at it! This is my portfolio website built on plain Vanilla JS/HTML/CSS, displaying all of my previously completed projects. In the future I would like to host this page on a more dynamic platform so it can update automatically when I publish a new project. As of right now, it's hosted on github pages, which only offers static webpage hosting, meaning no databases to hold my projects.",
-        "thumbnail": "./resources/Portfolio.png",
+        "thumbnail": "./thumbnails/Portfolio.png",
         "weblink": "https://jessegroves123.github.io/Portfolio/",
         "repolink": "https://github.com/jessegroves123/Portfolio"
     },
@@ -36,7 +36,7 @@ data = `
             "magic"
         ],
         "description": "A magical Glyph drawing game/tool using SVG to accept user-drawn, symmetrical, circular, smooth shaped 'glyphs', which will then be combined with other glyphs in the same drawing to produce a chemical-reaction-like effect, but magical!",
-        "thumbnail": "./resources/GlyphCrafter.png",
+        "thumbnail": "./thumbnails/GlyphCrafter.png",
         "weblink": "https://jessegroves123.github.io/GlyphCrafter/",
         "repolink": "https://github.com/jessegroves123/GlyphCrafter"
     }
