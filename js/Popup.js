c0d731a5d2f6ac7582cb281a275cf58d0ca98b20
Jesse Groves <grovesjesse@gmail.com>
2023-08-30 18:53:25 -0400

    experimental popup object

--- js/Popup.js
+++ js/Popup.js
@@ -1,5 +1,9 @@
 class Popup {
     constructor() {
-
+        this.element = this.createElement();
+    }
+    createElement() {
+        let element = document.createElement('div')
+        return element;
     }
 }
